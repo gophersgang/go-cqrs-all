87fcfdcc8e3f89f6a842ade81bdd172c6360015e
Roman Heinrich <roman.heinrich@gmail.com>
2017-03-08 14:58:05 +0100

    updates crawler

--- crawler/main.go
+++ crawler/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"regexp"
 	"sort"
 	"strings"
 	"sync"
@@ -12,28 +14,10 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
-type repoInfo struct {
-	url         string
-	description string
-	lastcommit  string
-}
-
-func (ri repoInfo) Markdown() string {
-	shortcommit := ri.lastcommit[0:10]
-	return fmt.Sprintf("- %s - %s <br/> ( %s )", ri.url, ri.description, shortcommit)
-}
-
-type reposByLastcommit []repoInfo
-
-func (ris reposByLastcommit) Len() int           { return len(ris) }
-func (ris reposByLastcommit) Less(i, j int) bool { return ris[i].lastcommit > ris[j].lastcommit }
-func (ris reposByLastcommit) Swap(i, j int)      { ris[i], ris[j] = ris[j], ris[i] }
-
-type reposByUrl []repoInfo
-
-func (ris reposByUrl) Len() int           { return len(ris) }
-func (ris reposByUrl) Less(i, j int) bool { return ris[i].url < ris[j].url }
-func (ris reposByUrl) Swap(i, j int)      { ris[i], ris[j] = ris[j], ris[i] }
+var (
+	urlFile    = "data/urls.txt"
+	readmeFile = "Readme.md"
+)
 
 func main() {
 	var wg sync.WaitGroup
@@ -45,31 +29,107 @@ func main() {
 		go func() {
 			defer wg.Done()
 			repo := process(a)
-			// fmt.Println(repo)
 			repos = append(repos, repo)
 			fmt.Print(".")
 		}()
 	}
 	wg.Wait()
+	printSortedAlpha(repos)
+	printSortedLastcommit(repos)
+	rh := readmeHandler{}
+	rh.replaceProjects(repos)
+	rh.replaceActivity(repos)
+}
 
-	sort.Sort(reposByUrl(repos))
+func printSortedAlpha(repos []repoInfo) {
+	sort.Sort(reposByURL(repos))
 	fmt.Print("\n\n")
 	for _, r := range repos {
 		fmt.Println(r.Markdown())
 	}
 }
 
+func printSortedLastcommit(repos []repoInfo) {
+	sort.Sort(reposByLastcommit(repos))
+	fmt.Print("\n\n")
+	for _, r := range repos {
+		fmt.Println(r.MarkdownActivity())
+	}
+}
+
 func process(url string) repoInfo {
-	doc := getDoc(url)
+	parser := repoParser{}
+	doc := parser.getDoc(url)
 	repo := repoInfo{
 		url:         strings.ToLower(url),
-		description: getDescription(doc),
-		lastcommit:  getLastcommit(doc),
+		description: parser.getDescription(doc),
+		lastcommit:  parser.getLastcommit(doc),
 	}
 	return repo
 }
 
-func getDescription(doc *goquery.Document) string {
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+/*
+	readmeHandler logic
+	rough translation of https://github.com/mindreframer/techwatcher/blob/master/_sh/logic.rb
+*/
+
+type readmeHandler struct{}
+
+func (rh readmeHandler) replaceProjects(repos []repoInfo) {
+	pattern := `(?s)<!-- PROJECTS_LIST -->(.*)<!-- /PROJECTS_LIST -->`
+	regexStart := `<!-- PROJECTS_LIST -->`
+	regexEnd := `<!-- /PROJECTS_LIST -->`
+
+	sort.Sort(reposByURL(repos))
+	lines := []string{}
+	lines = append(lines, regexStart)
+	for _, repo := range repos {
+		lines = append(lines, repo.Markdown())
+	}
+	lines = append(lines, regexEnd)
+
+	r := regexp.MustCompile(pattern)
+	// fmt.Println(r)
+
+	data, err := ioutil.ReadFile(readmeFile)
+	check(err)
+	res := r.ReplaceAllString(string(data), strings.Join(lines, "\n"))
+	ioutil.WriteFile(readmeFile, []byte(res), 0777)
+}
+
+func (rh readmeHandler) replaceActivity(repos []repoInfo) {
+	pattern := `(?s)<!-- ACTIVITY_LIST -->(.*)<!-- /ACTIVITY_LIST -->`
+	regexStart := `<!-- ACTIVITY_LIST -->`
+	regexEnd := `<!-- /ACTIVITY_LIST -->`
+
+	sort.Sort(reposByLastcommit(repos))
+	lines := []string{}
+	lines = append(lines, regexStart)
+	for _, repo := range repos {
+		lines = append(lines, repo.MarkdownActivity())
+	}
+	lines = append(lines, regexEnd)
+	r := regexp.MustCompile(pattern)
+	// fmt.Println(r)
+
+	data, err := ioutil.ReadFile(readmeFile)
+	check(err)
+	res := r.ReplaceAllString(string(data), strings.Join(lines, "\n"))
+	ioutil.WriteFile(readmeFile, []byte(res), 0777)
+}
+
+/*
+	Repo Parser logic
+*/
+type repoParser struct{}
+
+func (r repoParser) getDescription(doc *goquery.Document) string {
 	var content string
 	doc.Find("meta").Each(func(i int, s *goquery.Selection) {
 		name, _ := s.Attr("name")
@@ -80,14 +140,14 @@ func getDescription(doc *goquery.Document) string {
 	return content
 }
 
-func getLastcommit(doc *goquery.Document) string {
-	if hasIncludedLastcommit(doc) {
-		return getLastcommitIncluded(doc)
+func (r repoParser) getLastcommit(doc *goquery.Document) string {
+	if r.hasIncludedLastcommit(doc) {
+		return r.getLastcommitIncluded(doc)
 	}
-	return getLastcommitAjax(doc)
+	return r.getLastcommitAjax(doc)
 }
 
-func hasIncludedLastcommit(doc *goquery.Document) bool {
+func (r repoParser) hasIncludedLastcommit(doc *goquery.Document) bool {
 	found := true
 	doc.Find(".commit-loader").Each(func(i int, s *goquery.Selection) {
 		found = false
@@ -95,7 +155,7 @@ func hasIncludedLastcommit(doc *goquery.Document) bool {
 	return found
 }
 
-func getLastcommitIncluded(doc *goquery.Document) string {
+func (r repoParser) getLastcommitIncluded(doc *goquery.Document) string {
 	var datetime string
 	doc.Find(".commit-tease relative-time").Each(func(i int, s *goquery.Selection) {
 		datetime, _ = s.Attr("datetime")
@@ -103,28 +163,28 @@ func getLastcommitIncluded(doc *goquery.Document) string {
 	return datetime
 }
 
-func getLastcommitAjax(doc *goquery.Document) string {
+func (r repoParser) getLastcommitAjax(doc *goquery.Document) string {
 	// extract the ajax url
 	// e.g.: <include-fragment class="commit-tease commit-loader" src="/f2prateek/coi/tree-commit/866dee22e2b11dd9780770c00bae53886d9b4863">
 	s := doc.Find(".commit-loader")
 	path, _ := s.Attr("src")
 	url := "https://github.com" + path
-	ajaxDoc := urlDoc(url)
-	return getLastcommit(ajaxDoc)
+	ajaxDoc := r.urlDoc(url)
+	return r.getLastcommit(ajaxDoc)
 }
 
-func getDoc(url string) *goquery.Document {
-	return urlDoc(url)
+func (r repoParser) getDoc(url string) *goquery.Document {
+	return r.urlDoc(url)
 	// return localDoc()
 }
 
-func urlDoc(url string) *goquery.Document {
+func (r repoParser) urlDoc(url string) *goquery.Document {
 	doc, err := goquery.NewDocument(url)
 	check(err)
 	return doc
 }
 
-func localDoc() *goquery.Document {
+func (r repoParser) localDoc() *goquery.Document {
 	filename := "crawler/fixture.html"
 	file, err := os.Open(filename)
 	check(err)
@@ -133,19 +193,48 @@ func localDoc() *goquery.Document {
 	return doc
 }
 
-func check(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
+/*
+	repoInfo logic
+*/
+type repoInfo struct {
+	url         string
+	description string
+	lastcommit  string
 }
 
-func loadUrls() []string {
-	return file2lines("data/urls.txt")
+func (ri repoInfo) Markdown() string {
+	lastcommit := ri.lastcommit[0:10]
+	shorturl := strings.Replace(ri.url, "https://github.com/", "", -1)
+	return fmt.Sprintf("- [%s](%s) - %s <br/> ( %s )", shorturl, ri.url, ri.description, lastcommit)
 }
+
+func (ri repoInfo) MarkdownActivity() string {
+	lastcommit := ri.lastcommit[0:10]
+	shorturl := strings.Replace(ri.url, "https://github.com/", "", -1)
+	link := fmt.Sprintf("[%s](%s)", shorturl, ri.url)
+	return fmt.Sprintf("- %s - %s  <br/> %s", lastcommit, link, ri.description)
+}
+
+type reposByLastcommit []repoInfo
+
+func (ris reposByLastcommit) Len() int           { return len(ris) }
+func (ris reposByLastcommit) Less(i, j int) bool { return ris[i].lastcommit > ris[j].lastcommit }
+func (ris reposByLastcommit) Swap(i, j int)      { ris[i], ris[j] = ris[j], ris[i] }
+
+type reposByURL []repoInfo
+
+func (ris reposByURL) Len() int           { return len(ris) }
+func (ris reposByURL) Less(i, j int) bool { return ris[i].url < ris[j].url }
+func (ris reposByURL) Swap(i, j int)      { ris[i], ris[j] = ris[j], ris[i] }
 
 /*
-simple lines reader
+	data loading (simple lines reader)
 */
+
+func loadUrls() []string {
+	return file2lines(urlFile)
+}
+
 func file2lines(filePath string) []string {
 	f, err := os.Open(filePath)
 	if err != nil {
@@ -168,5 +257,5 @@ func file2lines(filePath string) []string {
 }
 
 func validURL(l string) bool {
-	return !strings.Contains(l, " ") && len(l) != 0
+	return !strings.Contains(l, " ") && len(l) != 0 && !strings.Contains(l, "#")
 }
